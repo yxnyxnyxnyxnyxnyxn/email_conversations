9f98832514e7280fb90dc2a2f8986c076bb6b983
yxnyxnyxnyxnyxnyxn <yxnyxnyxnyxnyxnyxn@gmail.com>
2018-07-26 15:20:51 -0400

    fixed bug

--- n_ary_tree.py
+++ n_ary_tree.py
@@ -1,3 +1,7 @@
+
+
+# Node class: store each email as a node
+# key: message id; children: emails reply to this message id
 class Node:
     def __init__(self,key, children =None):
         self.key = key
@@ -6,15 +10,19 @@ class Node:
     def __str__(self):
         return str(self.key)
 
-
+# N_ary_Tree class: create conversation, group messages related to each other
 class N_ary_Tree:
     
     def __init_(self):
         self.root = None
         self.size = 0
     
+    # Find the 'In-Reply-To' message by id
+    # key: 'In-Reply-To' message id
+    # node: start point
     def find_node(self,node,key):
-        if node == None or node.key == key:
+       
+        if  node == None or node.key == key:
             return node
         
         for child in node.children:
@@ -22,35 +30,41 @@ class N_ary_Tree:
             if found:
                 return found
 
-        return None
 
-
-    def add(self,new_key,parent_key= None):
-        new_node =Node(new_key)
+    
+   
+    
+    # Function to add child to node
+    # parent_key: 'In-Reply-To' message id
+    # new_key: message id of email needed to be add to the tree
+    def add(self,new_key,parent_key=None):
+        
+        new_node = Node(new_key)
         if parent_key == None:
             self.root = new_node
-    
         else:
             parent_node = self.find_node(self.root,parent_key)
             parent_node.children.append(new_node)
 
 
-
-
+    # dfs function to loop through each branches
+    # node: root
     def dfs(self,node):
         path = []
+        # visit node recursively
         def recurse(n):
             path.append(n.key)
             if not n.children:
                 yield path
             for child in n.children:
-                for x in recurse(child):
-                    yield x
+                for c in recurse(child):
+                    yield c
             path.pop()
-        for v in recurse(node):
+        for n in recurse(node):
             yield path
-        
 
+        
+    # Function to print each branch node to leaf
     def print_branches(self,node):
         for path in self.dfs(self.root):
             path_str = '<-'.join(map(str,[n for n in path]))
@@ -59,14 +73,3 @@ class N_ary_Tree:
 
 
 
-if __name__ == "__main__":
-    				
-	tree = N_ary_Tree()
-	tree.add('10')
-	tree.add('20', '10')
-	tree.add('30', '10')
-	tree.add('60', '20')
-	
-
-	
-	print tree.print_branches(tree)
